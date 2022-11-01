9f7927c95c07728055522165bfc1baf51c8d72ee
Katherine Abrikian <kabrikian@gmail.com>
2022-11-01 14:38:24 -0400

    Read all books

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,27 @@
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-hello_world_bp = Blueprint("hello_world_bp", __name__)
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
 
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    return "Hello, World!"
+books = [
+    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+]
 
-
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    return {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-
-
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
